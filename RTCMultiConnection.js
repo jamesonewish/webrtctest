9104fbb5178c2d5bec96209a935fdf1dbcd71811
jamesonewish <32664535+jamesonewish@users.noreply.github.com>
2018-02-21 23:51:39 +0700

    พพ
    พพ

--- RTCMultiConnection.js
+++ RTCMultiConnection.js
@@ -3893,7 +3893,7 @@
         function getIceServers(connection) {
             var iceServers = [];
 
-            //iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
+            iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
 
             //iceServers.push(getTURNObj('stun:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
             //iceServers.push(getTURNObj('turn:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
@@ -3907,7 +3907,7 @@
 
             // check if restund is still active: http://webrtcweb.com:4050/
             //iceServers.push(getTURNObj('stun:webrtcweb.com:4455', 'muazkh', 'muazkh')); // restund
-            iceServers.push(getTURNObj('turn:webrtcweb.com:4455', 'muazkh', 'muazkh')); // restund
+            //iceServers.push(getTURNObj('turn:webrtcweb.com:4455', 'muazkh', 'muazkh')); // restund
             //iceServers.push(getTURNObj('turn:webrtcweb.com:5544?transport=tcp', 'muazkh', 'muazkh')); // restund
 
             return iceServers;
