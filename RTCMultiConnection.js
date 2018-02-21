4ebd35f3f3b4bb5a1f3fe07cf917743c86fc75e7
jamesonewish <32664535+jamesonewish@users.noreply.github.com>
2018-02-21 23:09:51 +0700

    ty
    ty

--- RTCMultiConnection.js
+++ RTCMultiConnection.js
@@ -3895,9 +3895,9 @@
 
             //iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
 
-            iceServers.push(getTURNObj('stun:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
+            //iceServers.push(getTURNObj('stun:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
             //iceServers.push(getTURNObj('turn:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
-            //iceServers.push(getTURNObj('turn:webrtcweb.com:8877', 'muazkh', 'muazkh')); // coTURN
+            iceServers.push(getTURNObj('turn:webrtcweb.com:8877', 'muazkh', 'muazkh')); // coTURN
 
             //iceServers.push(getTURNObj('turns:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
             //iceServers.push(getTURNObj('turns:webrtcweb.com:8877', 'muazkh', 'muazkh')); // coTURN
